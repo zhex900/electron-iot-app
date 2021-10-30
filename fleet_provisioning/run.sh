d3d7c5c048185bb26f5cb2f9ed7304c922c39565
Jake He <zhex900@gmail.com>
2021-10-30 19:40:23 +1100

    remove fixed valued iot endpoint

--- fleet_provisioning/run.sh
+++ fleet_provisioning/run.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
+ENDPOINT=$(aws iot describe-endpoint --endpoint-type iot:Data-ATS | jq -r '.endpointAddress')
+
 node dist/index.js \
-        --endpoint a29w0hvardstv-ats.iot.ap-southeast-2.amazonaws.com \
+        --endpoint $ENDPOINT \
         --ca_file ../infra/AmazonRootCA1.pem \
         --cert ../infra/claim.cert.pem \
         --key ../infra/claim.private.key \
